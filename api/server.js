ae3ee5d7dc18fe47f5b409cf33d0e446603f4cf7
samilkafa <samilkafa@gmail.com>
2023-08-11 14:49:44 +0300

    Project initial setup completed

---
+++ api/server.js
@@ -0,0 +1,17 @@
+// 1. imports
+const express = require("express");
+const server = express();
+
+// 2. global middlewares
+
+
+// 3. routers
+server.get("/", (req, res) => {
+    res.json({message: "Server up and running..."});
+  });
+
+// 4. error middlwares
+
+
+// 5. exports
+module.exports = server;
