73865f91a0176ab81b4d171b80efb219aaef328c
samilkafa <samilkafa@gmail.com>
2023-08-11 15:17:27 +0300

    Backend security measures implemented

--- api/server.js
+++ api/server.js
@@ -2,9 +2,14 @@
 const express = require("express");
 const server = express();
 require("dotenv").config();
+const helmet = require("helmet")
+const cors = require("cors");
+const morgan = require("morgan");
 
 // 2. global middlewares
-
+server.use(helmet());       // 3rd party middleware
+server.use(cors());         // 3rd party middleware
+server.use(morgan("dev"));  // 3rd party middleware
 
 // 3. routers
 server.get("/", (req, res) => {
